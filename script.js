eb9c3b2822860fec889ed33f2bda9f5f560d6ca1
BabyRamboDrake <francisdrake.adv@gmail.com>
2023-02-13 23:35:30 +0100

    5th commit

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let addQuestBtn = document.getElementById("add-quest");
 let pullQuestBtn = document.getElementById("pull-quest");
 let selectedQuestDiv = document.getElementById("selected-quest");
 let quests = [];
+let experiencePoints = 0;
+let level = 1;
 
 addQuestBtn.addEventListener("click", function addQuest() {
     let quest = questInput.value;
@@ -30,11 +32,22 @@ const completeQuestButton = document.querySelector("#complete-quest");
 completeQuestButton.addEventListener("click", function completeQuest() {
   const selectedQuest = document.querySelector("#selected-quest");
   if (selectedQuest.textContent) {
-    // Award the points
-    const currentPoints = parseInt(document.querySelector("#points").textContent);
-    document.querySelector("#points").textContent = currentPoints + 10;
+    experiencePoints += 25;
+    let experienceBar = document.getElementById("experience-points");
+    experienceBar.style.width = experiencePoints + "%";
+
+    let levelDisplay = document.getElementById("level");
+    let levelThreshold = 100;
+    if (experiencePoints >= levelThreshold) {
+      level++;
+      levelDisplay.innerHTML = level;
+      experiencePoints = 0;
+      experienceBar.style.width = experiencePoints + "%"
+    } else {
+        experienceBar.style.width = experiencePoints + "%";
+      }
+    }
 
     // Remove the selected quest
     selectedQuest.textContent = "";
-  }
 });
