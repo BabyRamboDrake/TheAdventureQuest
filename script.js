4a9460c8a6694a2d8a2e031acb52595a0c9fc2ff
BabyRamboDrake <francisdrake.adv@gmail.com>
2023-02-27 10:16:49 +0100

    Updates to timer and localstorage

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
-
 let questInput = document.getElementById("quest-input");
 let addQuestBtn = document.getElementById("add-quest");
 let pullQuestBtn = document.getElementById("pull-quest");
 let selectedQuestDiv = document.getElementById("selected-quest");
-let quests = [];
+let quests = JSON.parse(localStorage.getItem("quests")) || [];
 let experiencePoints = 0;
-let level = 1;
+let level = JSON.parse(localStorage.getItem("level")) || 1;
 
 addQuestBtn.addEventListener("click", function addQuest() {
     let quest = questInput.value;
     if (quest) {
         quests.push(quest);
         questInput.value = "";
+        localStorage.setItem("quests", JSON.stringify(quests));
     }
 });
 
@@ -22,8 +22,9 @@ pullQuestBtn.addEventListener("click", function pullQuest() {
         let selectedQuest = quests[randomIndex];
         selectedQuestDiv.innerHTML = selectedQuest;
         quests.splice(randomIndex, 1);
+        localStorage.setItem("quests", JSON.stringify(quests));
     } else {
-        selectedQuestDiv.innerHTML = "Find new Quest";
+        selectedQuestDiv.innerHTML = "Add new Quest";
     }
 });
 
@@ -43,11 +44,21 @@ completeQuestButton.addEventListener("click", function completeQuest() {
       levelDisplay.innerHTML = level;
       experiencePoints = 0;
       experienceBar.style.width = experiencePoints + "%"
+      localStorage.setItem("level", JSON.stringify(level));
+      localStorage.setItem("quests", JSON.stringify([]));
     } else {
-        experienceBar.style.width = experiencePoints + "%";
-      }
+      experienceBar.style.width = experiencePoints + "%";
     }
+  }
 
-    // Remove the selected quest
-    selectedQuest.textContent = "";
+  // Remove the selected quest
+  selectedQuest.textContent = "";
 });
+
+// Initialize the level and quests from localStorage
+let levelDisplay = document.getElementById("level");
+levelDisplay.innerHTML = level;
+let experienceBar = document.getElementById("experience-points");
+experienceBar.style.width = experiencePoints + "%";
+
+
